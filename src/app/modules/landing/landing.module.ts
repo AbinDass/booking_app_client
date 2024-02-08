a15e6321d56aed47085cb3cb43ac4d4a51b14de7
AbinDass <abindas350@gmail.com>
2024-02-08 16:18:23 +0530

    developing the user interface of landing page and created a shared module and reusable components inside the module

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    LandingComponent  
+    LandingComponent ,
+
+
   ],
   imports: [
     CommonModule,
     LandingRoutingModule,
+    SharedModule
     
   ]
 })
